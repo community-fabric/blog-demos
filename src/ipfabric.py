9b0d055d2f4e97020a41ab1ffeb04cf20aa7f3b4
Justin Jeffery <justin.jeffery@ipfabric.io>
2021-11-10 13:52:23 -0500

    Moved limit to init

--- src/ipfabric.py
+++ src/ipfabric.py
@@ -5,18 +5,20 @@ from urllib.parse import urljoin
 
 
 class IPFabric(Session):
-    def __init__(self, base_url: str, token: str, verify: bool = True, api_version: str = "v1"):
+    def __init__(self, base_url: str, token: str, verify: bool = True, api_version: str = "v1", limit: int = 10000):
         """
         Initiate IPFabric session
         :param base_url: str: https://your_instance.ipfabric.io
         :param token: str: Your API Token
         :param verify: bool: False to ignore certificate verification
         :param api_version: str: Default to v1 the current version
+        :param limit: int: Limit of number of entries to return in one call
         """
         super(IPFabric, self).__init__()
         self.base_url = urljoin(urljoin(base_url, "api/"), api_version + '/')
         self.headers.update({'Content-Type': 'application/json', 'X-API-Token': token})
         self.verify = verify
+        self.limit = limit
 
     def query(self, endpoint: str, payload: dict):
         """
@@ -32,7 +34,6 @@ class IPFabric(Session):
             url: str,
             payload: dict,
             data: Optional[Union[list, None]] = None,
-            limit: int = 10000,
             start: int = 0
     ):
         """
@@ -40,20 +41,19 @@ class IPFabric(Session):
         :param url: str: Full URL to post to
         :param payload: dict: Data to submit to IP Fabric
         :param data: list: List of data to append subsequent calls
-        :param limit: int: Limit of number of entries to return in one call
         :param start: int: Where to start for the data
         :return: list: List of dictionaries
         """
         data = list() if not data else data
 
         payload["pagination"] = dict(
-            limit=limit,
+            limit=self.limit,
             start=start
         )
         r = self.post(url, json=payload)
         r.raise_for_status()
         r = r.json()
         data.extend(r['data'])
-        if limit + start < r["_meta"]["count"]:
-            self._ipf_pager(url, payload, data, limit, start+limit)
+        if self.limit + start < r["_meta"]["count"]:
+            self._ipf_pager(url, payload, data, start+self.limit)
         return data
